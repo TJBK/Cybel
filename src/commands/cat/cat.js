bd24f75fb57efdd8b4dd1980665e87380dd1d57f
TJBK <martin@desu.tech>
2017-08-21 17:19:41 +0100

    Add the cat command

---
+++ src/commands/cat/cat.js
@@ -0,0 +1,30 @@
+import request from 'request'
+
+let commands = {
+  name: 'cat',
+  use: '<command>',
+  desc: 'Get a random cat pic/vid',
+  process: async (msg, suffix, client, serverDoc, db, utl) => {
+    await request({url: 'http://random.cat/meow'}, async (err, res, json) => {
+      if (err) console.error(err)
+      let cat
+      if (!err && res.statusCode === 200) {
+        cat = await JSON.parse(json)
+      }
+      let embed = {}
+      let vid = false
+      if (cat.file.indexOf('mp4') !== -1) vid = true
+      embed = {
+        description: 'Get kittie [here](' + cat.file + ')',
+        image: {
+          url: cat.file
+        }
+      }
+      if (vid) embed.description = 'Get kittie video [here](' + cat.file + ')'
+      await msg.delete()
+      await msg.channel.send({embed: embed})
+    })
+  }
+}
+
+export {commands}
