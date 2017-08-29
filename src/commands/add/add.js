b307603d68111f8c361beb6efb46e3b34954a8f0
TJBK <martin@desu.tech>
2017-08-29 22:33:57 +0100

    Come on

--- src/commands/add/add.js
+++ src/commands/add/add.js
@@ -5,31 +5,34 @@ let commands = {
   process: async (msg, suffix, client, serverDoc, db, utl) => {
     let user = msg.guild.members.array()
     let role = msg.guild.roles.array()
-    let abuse = await false
+    let abuse = false
     if (suffix === '@everyone' || suffix === '@here') {
-      abuse = await true
+      abuse = true
     }
     for (let i in user) {
       let users = user[i]
-      if (suffix.includes(users)) abuse = await true; break
+      if (suffix.includes(users)) abuse = true; break
     }
     for (let i in role) {
       let roles = role[i]
-      if (suffix.includes(roles)) abuse = await true; break
+      if (suffix.includes(roles)) abuse = true; break
     }
-    if (abuse) msg.reply('Sorry ' + suffix + ' is not allowed')
-    try {
-      let userID = msg.author.id
-      let s = new db.SaysDB({
-        auther: userID,
-        say: suffix
-      })
-      s.save((s) => {
-        msg.delete()
-        msg.channel.send('I have stored ' + suffix + ' congrats').then(message => message.delete({timeout: 60000}))
-      })
-    } catch (err) {
-      msg.channel.send('Fail' + err).then(message => message.delete({timeout: 60000}))
+    if (abuse) {
+      msg.reply('Sorry ' + suffix + ' is not allowed')
+    } else {
+      try {
+        let userID = msg.author.id
+        let s = new db.SaysDB({
+          auther: userID,
+          say: suffix
+        })
+        s.save((s) => {
+          msg.delete()
+          msg.channel.send('I have stored ' + suffix + ' congrats').then(message => message.delete({timeout: 60000}))
+        })
+      } catch (err) {
+        msg.channel.send('Fail' + err).then(message => message.delete({timeout: 60000}))
+      }
     }
   }
 }
