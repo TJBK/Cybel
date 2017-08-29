ae878fc9febfe2f63bacaae12623656c3d746c88
TJBK <martin@desu.tech>
2017-08-29 22:38:30 +0100

    Okay I'll fix another time

--- src/commands/add/add.js
+++ src/commands/add/add.js
@@ -3,6 +3,9 @@ let commands = {
   use: '<command> <arg>',
   desc: 'Add something to the bot to be pulled later',
   process: async (msg, suffix, client, serverDoc, db, utl) => {
+    msg.channel.send('Sorry I am disabled')
+    let disabled = true
+    if (disabled === true) return
     let user = msg.guild.members.array()
     let role = msg.guild.roles.array()
     let abuse = false
@@ -17,22 +20,19 @@ let commands = {
       let roles = role[i]
       if (suffix.includes(roles)) abuse = true; break
     }
-    if (abuse) {
-      msg.reply('Sorry ' + suffix + ' is not allowed')
-    } else {
-      try {
-        let userID = msg.author.id
-        let s = new db.SaysDB({
-          auther: userID,
-          say: suffix
-        })
-        s.save((s) => {
-          msg.delete()
-          msg.channel.send('I have stored ' + suffix + ' congrats').then(message => message.delete({timeout: 60000}))
-        })
-      } catch (err) {
-        msg.channel.send('Fail' + err).then(message => message.delete({timeout: 60000}))
-      }
+    if (abuse) return msg.reply('Sorry ' + suffix + ' is not allowed')
+    try {
+      let userID = msg.author.id
+      let s = new db.SaysDB({
+        auther: userID,
+        say: suffix
+      })
+      s.save((s) => {
+        msg.delete()
+        msg.channel.send('I have stored ' + suffix + ' congrats').then(message => message.delete({timeout: 60000}))
+      })
+    } catch (err) {
+      msg.channel.send('Fail' + err).then(message => message.delete({timeout: 60000}))
     }
   }
 }
