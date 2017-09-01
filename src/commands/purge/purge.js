d3073a7cbd28f6c4c005e0c927655034840aafb8
TJBK <martin@desu.tech>
2017-09-01 10:45:04 +0100

    Update purge to use the new way

--- src/commands/purge/purge.js
+++ src/commands/purge/purge.js
@@ -4,10 +4,10 @@ let commands = {
   desc: 'For admins to delete many messages at once',
   process: async (msg, suffix, client, serverDoc, db, utl) => {
     if (!utl.isAdmin(msg.member)) return msg.reply('Sorry you don\'t have perms for that').then(message => message.delete({timeout: 60000})).catch(console.error)
-    let deleteCount = await parseInt(suffix)
+    let deleteCount = parseInt(suffix)
     if (!deleteCount || deleteCount < 2 || deleteCount > 100) return msg.reply('Please provide a number between 2 and 100 for the number of messages to delete')
     try {
-      msg.channel.fetchMessages({ limit: deleteCount }).then(messages => msg.channel.bulkDelete(messages))
+      msg.channel.messages.fetch({ limit: deleteCount }).then(messages => msg.channel.bulkDelete(messages))
     } catch (err) {
       msg.channel.send('Fail' + err).then(message => message.delete({timeout: 60000}))
     }
